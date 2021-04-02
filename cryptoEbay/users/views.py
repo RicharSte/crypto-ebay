c3fe4f614d08f60a895412abe741f443451383b2
Stepabov Nikolai <Data.Science.NikSte@yandex.ru>
2021-04-02 18:38:52 +0300

    Добавленна возможность обнавлять Профиль и фото

--- cryptoEbay/users/views.py
+++ cryptoEbay/users/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
-from .forms import UserRegiesterForm 
+from .forms import(
+    UserRegiesterForm,
+    UserUpdateForm,
+    ProfileUpdateForm
+)
 
 def register(request):
-    print(request.method)
     if request.method == 'POST':
         form = UserRegiesterForm(request.POST)
         if form.is_valid():
@@ -19,4 +22,25 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, 
+                                   request.FILES,
+                                   instance=request.user.profile)
+        
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your account has been updated!')
+            return redirect('profile')
+    
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+        
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    
+    return render(request, 'users/profile.html', context)
