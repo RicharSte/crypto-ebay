3dfa7a4c582688e2d2f8c9607ae0a7842b0f4aca
Nikolai Stepanov <nikolaistepanov@MacBook-Air-Nikolai.local>
2021-04-15 22:51:24 +0300

    Создана таблица для сохранения цены крипты

--- cryptoEbay/marketplace/models.py
+++ cryptoEbay/marketplace/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from datetime import date
+
+class CoinPriseModel(models.Model):
+    name = models.CharField(max_length=8)
+    price = models.IntegerField()
+    date = models.DateField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return self.name
